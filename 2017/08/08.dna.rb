ce1da98ff8be3bc0e738255331984e017c76f4aa
Abhinav Tushar <lepisma@fastmail.com>
2017-12-08 13:15:16 -0500

    Use defaultdict

--- 2017/08/08.dna.rb
+++ 2017/08/08.dna.rb
@@ -1,18 +1,16 @@
 # ribosome.rb 08.dna.rb | xargs -0 python -c
 
 lines = File.readlines('input.txt')
-.regs = set()
-lines.each do |line|
-.regs.add("@{line.split(' ')[0]}")
-end
-.regs = [[r, 0] for r in regs]
-.names = [r[0] for r in regs]
+.from collections import defaultdict
+.regs = defaultdict(int)
 .mem = []
 lines.each do |line|
-s = line.split(' ')
-.regs[names.index("@{s[0]}")][1] @{(s[1] === 'inc') ? '+=' : '-='} @{s[2]}
-./+ if regs[names.index("@{s[4]}")][1] @{s[5]} @{s[6]} else 0
-.mem += [r[1] for r in regs]
+  s = line.split(' ')
+  s[0] = "regs['#{s[0]}']"
+  s[4] = "regs['#{s[4]}']"
+  s[1] = s[1] == 'inc' ? '+=' : '-='
+.@{s.join" "} else 0
+.mem += [r for r in regs.values()]
 end
-.print(f"Part one: {max([r[1] for r in regs])}")
+.print(f"Part one: {max([r for r in regs.values()])}")
 .print(f"Part two: {max(mem)}")
