649c36db3daa71b8b6651c6a0f98fb73df54eed9
Abhinav Tushar <lepisma@fastmail.com>
2017-12-10 03:37:23 -0500

    Use vectors

--- 2017/10/10.lisp
+++ 2017/10/10.lisp
@@ -6,36 +6,39 @@
 (defun repeat (sequence times)
   (apply #'concatenate 'list (make-list times :initial-element sequence)))
 
-(defun reverse-subseq (sequence from len &optional (at 0))
-  (if (= at (floor len 2)) sequence
-      (let ((x (mod (+ from at) (length sequence)))
-            (y (mod (- (+ from len) (+ at 1)) (length sequence))))
-        (rotatef (nth x sequence) (nth y sequence))
-        (reverse-subseq sequence from len (+ at 1)))))
+(defun arange (limit)
+  (make-array limit :initial-contents (alexandria:iota limit :start 0)))
+
+(defun reverse-subarray (array from len &optional (at 0))
+  (if (= at (floor len 2)) array
+      (let ((x (mod (+ from at) (length array)))
+            (y (mod (- (+ from len) (+ at 1)) (length array))))
+        (rotatef (aref array x) (aref array y))
+        (reverse-subarray array from len (+ at 1)))))
 
 (defun reduce-slice (sequence slice-size func &optional acc)
-  (if (null sequence) (nreverse acc)
-      (reduce-slice (nthcdr slice-size sequence) slice-size func
-                    (cons (apply func (subseq sequence 0 slice-size)) acc))))
+  (if (= (length sequence) 0) (nreverse acc)
+      (reduce-slice (subseq sequence slice-size) slice-size func
+                    (cons (apply func (nthcdr slice-size sequence)) acc))))
 
-(defun update-numbers (numbers lengths &optional (pos 0) (skip 0))
-  (if (null lengths) (values numbers pos skip)
-      (update-numbers (reverse-subseq numbers pos (car lengths))
-                      (cdr lengths) (+ pos (car lengths) skip) (+ skip 1))))
+(defun update-array (array lengths &optional (pos 0) (skip 0))
+  (if (null lengths) array
+      (update-array (reverse-subarray array pos (car lengths))
+                    (cdr lengths) (+ pos (car lengths) skip) (+ skip 1))))
 
 ;; Part one
 (let* ((lengths (mapcar #'parse-integer
                         (cl-ppcre:split "," (alexandria:read-file-into-string "input.txt"))))
-       (numbers (alexandria:iota 256 :start 0))
-       (results (update-numbers numbers lengths)))
-  (* (car results) (cadr results)))
+       (numbers (arange 256))
+       (results (update-array numbers lengths)))
+  (* (aref results 0) (aref results 1)))
 
 ;; Part two
 (let* ((lengths (concatenate 'list
                              (butlast (map 'list #'identity (alexandria:read-file-into-byte-vector "input.txt")))
                              '(17 31 73 47 23)))
-       (numbers (alexandria:iota 256 :start 0))
-       (results (update-numbers numbers (repeat lengths 64))))
+       (numbers (arange 256))
+       (results (update-array numbers (repeat lengths 64))))
   (format nil "~{~(~A~)~}"
           (mapcar (lambda (x) (write-to-string x :base 16))
-                  (reduce-slice results 16 #'logxor))))
+                  (reduce-slice (map 'list #'identity results) 16 #'logxor))))
